12007b90a71069a8475c551fabef88685afa497f
PRPDSS <prpdss@gmail.com>
2024-12-16 14:55:56 +0300

    Выполнена 12 тетрадъ

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+function showMessage(msg) { console.log(msg) }
+function changeBackgroundColor() {
+    const newColor = '#' + Math.floor(Math.random() * 16777215).toString(16)
+    // console.log(newColor)
+    document.querySelector("main").style.backgroundColor = newColor
+}
+function toggleVisibility() {
+    let elem = document.getElementById("visty-change")
+    if (elem == null) return
+    elem.style.visibility = elem.style.visibility == "hidden" ? "visible" : "hidden"
+    console.log("работает??")
+}
+function logCurrentTime() {
+    const time = new Date()
+    alert("Сейчас " + time.getHours() + ':' + time.getMinutes())
+}
