4935ef012bf45c94c1250e39dbbc81d414f16e37
PRPDSS <prpdss@gmail.com>
2024-12-20 00:51:23 +0300

    Пофик, рабртает!

--- scripts/script.js
+++ scripts/script.js
@@ -15,3 +15,27 @@ function logCurrentTime() {
     const time = new Date()
     alert("Сейчас " + time.getHours() + ':' + time.getMinutes())
 }
+
+function rotateImg(event, element) {
+    // Получаем размеры контейнера
+    const rect = element.getBoundingClientRect();
+    const x = event.clientX - rect.left; // Координаты внутри контейнера
+    const y = event.clientY - rect.top;
+
+    // Вычисляем смещение для поворота
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+    const rotateX = ((y - centerY) / centerY) * 30; // Поворот по оси X
+    const rotateY = ((x - centerX) / centerX) * -30; // Поворот по оси Y
+    const tiltZ = ((x - centerX) / centerX) * 100; // Лёгкий наклон
+
+
+    // Применяем поворот к изображению
+    const img = element.querySelector('img');
+    img.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) translateZ(${tiltZ}px)`;
+};
+
+function restoreImg(element) {
+    const img = element.querySelector('img')
+    img.style.transform = 'rotateX(0deg) rotateY(0deg) translateZ(0px)';
+}
